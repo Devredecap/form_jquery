c39ed21f9578983d7a5844c7bff9ca96829d4a1f
Renata Alves <devredecap@gmail.com>
2024-03-13 16:36:44 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000-000-000-00')
+    $('#cep').mask('00000-000')
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome'
+        },
+        
+        submitHandler: function(form){
+            console.log(form)
+            $('form')[0].reset();
+            var mensagem = $("<p>").text("Cadastro realizado com sucesso");
+            mensagem.addClass("sucesso");
+            $("#container2").append(mensagem);
+            mensagem.show();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+        
+    }) 
+})
